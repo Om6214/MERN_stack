89dbb7123db3f9051acadd85e658693eb929b50a
Om6124 <omnathganapure9981@gmail.com>
2024-06-02 16:49:55 +0530

    added registeration page

---
+++ Client/src/pages/Login.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <h1>this is Login page</h1>
+  )
+}
+
+export default Login
---
+++ Client/src/pages/Register.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import regimg from '/reg.avif';
+import '../App.css';
+
+const Register = () => {
+  const [user, setUser] = useState({
+    Name: "",
+    Email: "",
+    Phone_number: "",
+    Password: ""
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser({
+      ...user,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(user);
+  };
+
+  return (
+    <div className="container">
+      <div className="img-fluid">
+        <img src={regimg} alt="Registration" />
+      </div>
+      <div className="form">
+        <h1>Registration Form</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="exampleInputName1" className="form-label">Name</label>
+            <input type="text" className="form-control" id="exampleInputName1" name="Name" value={user.Name} onChange={handleChange} />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+            <input type="email" className="form-control" id="exampleInputEmail1" name="Email" value={user.Email} onChange={handleChange} aria-describedby="emailHelp" />
+            <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputPhone1" className="form-label">Phone Number</label>
+            <input type="text" className="form-control" id="exampleInputPhone1" name="Phone_number" value={user.Phone_number} onChange={handleChange} />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+            <input type="password" className="form-control" id="exampleInputPassword1" name="Password" value={user.Password} onChange={handleChange} />
+          </div>
+          <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
