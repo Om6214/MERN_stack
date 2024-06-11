f7c0780ce44dbaf22b329a0aa6d47a362b7f32e2
Om6124 <omnathganapure9981@gmail.com>
2024-06-11 17:45:03 +0530

    changed domain

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const admin_router = require("./router/admin-router")
 // handling the cors error
 
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:'https://ommern-pxxxthypw-om6214s-projects.vercel.app',
     methods:"GET, POST, PUT, DELETE, PATCH, HEAD",
     credentials:true
 }
